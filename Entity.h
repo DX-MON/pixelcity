226b06b67638eac7c8e58c225f71995f7c090c2a
dx-mon <dx-mon@users.sourceforge.net>
2012-05-13 13:51:31 +0100

    Fixed up Entity to compile under Linux

--- Entity.h
+++ Entity.h
@@ -1,37 +1,56 @@
 #ifndef TYPES
-#include "glTypes.h"
+	#include "glTypes.h"
 #endif
 
 #ifndef ENTITY
 
-#define ENTITY
+	#define ENTITY
 
 class CEntity
 {
 private:
 protected:
 
-  GLvector                _center;
+	GLvector _center;
 
 public:
-                          CEntity (void);
-  virtual                 ~CEntity () {};
-  virtual void            Render (void);
-  virtual void            RenderFlat (bool wirefame);
-  virtual unsigned        Texture () { return 0; }
-  virtual void            Update (void);
-  virtual bool            Alpha () { return false; }
-  virtual int             PolyCount () { return 0; }
-  GLvector                Center () { return _center; }
+	CEntity(void);
+
+	virtual ~CEntity()
+	{
+	};
+	virtual void Render(void);
+	virtual void RenderFlat(bool wirefame);
+
+	virtual unsigned Texture()
+	{
+		return 0;
+	}
+	virtual void Update(void);
+
+	virtual bool Alpha()
+	{
+		return false;
+	}
+
+	virtual int PolyCount()
+	{
+		return 0;
+	}
+
+	GLvector Center()
+	{
+		return _center;
+	}
 
 };
 
-void      EntityClear ();
-int       EntityCount (void);
-float     EntityProgress ();
-bool      EntityReady ();
-void      EntityRender (void);
-void      EntityUpdate (void);
-int       EntityPolyCount (void);
+void EntityClear();
+int EntityCount(void);
+float EntityProgress();
+bool EntityReady();
+void EntityRender(void);
+void EntityUpdate(void);
+int EntityPolyCount(void);
 
 #endif
