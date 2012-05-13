51bf25efa902c18863ea54a109134de0cd586218
dx-mon <dx-mon@users.sourceforge.net>
2012-05-13 13:50:41 +0100

    Fixed up Car to compile under Linux

--- Car.h
+++ Car.h
@@ -1,30 +1,34 @@
 class CCar
 {
-  GLvector        m_position;
-  GLvector        m_drive_position;
-  bool            m_ready;
-  bool            m_front;
-  int             m_drive_angle;
-  int             m_row;
-  int             m_col;
-  int             m_direction;
-  int             m_change;
-  int             m_stuck;
-  float           m_speed;
-  float           m_max_speed;
+	GLvector m_position;
+	GLvector m_drive_position;
+	bool m_ready;
+	bool m_front;
+	int m_drive_angle;
+	int m_row;
+	int m_col;
+	int m_direction;
+	int m_change;
+	int m_stuck;
+	float m_speed;
+	float m_max_speed;
 
 public:
-                  CCar ();
-  bool            TestPosition (int row, int col);
-  void            Render ();
-  void            Update ();
-  void            Park () { m_ready = false;}
-  class CCar*     m_next;
+	CCar();
+	bool TestPosition(int row, int col);
+	void Render();
+	void Update();
+
+	void Park()
+	{
+		m_ready = false;
+	}
+	class CCar* m_next;
 
 };
 
-void  CarClear ();
-int   CarCount ();
-void  CarRender ();
-void  CarUpdate ();
+void CarClear();
+int CarCount();
+void CarRender();
+void CarUpdate();
 
