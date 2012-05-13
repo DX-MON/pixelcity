e2ad215294a0a315b0bf0b716f51c5bcad1a59e8
dx-mon <dx-mon@users.sourceforge.net>
2012-05-13 13:53:04 +0100

    Fixed up Sky to compile under Linux

--- Sky.cpp
+++ Sky.cpp
@@ -6,39 +6,49 @@
 
 -------------------------------------------------------------------------------
 
-  Did this need to be written as a class? It did not. There will never be 
+  Did this need to be written as a class? It did not. There will never be
   more than one sky in play, so the whole class structure here is superflous,
   but harmless.
-  
+
 -----------------------------------------------------------------------------*/
 
 #define SKYPOINTS      24
 
-#include <windows.h>
+#ifdef _WINDOWS
+	#include <windows.h>
+#endif
 #include <math.h>
-#include <gl\gl.h>
+#if defined(__MACOSX__)
+	#include <OpenGL/gl.h>
+#elif defined(__MACOS__)
+	#include <GL/gl.h>
+#else
+	#include <GL/gl.h>
+#include <GTK++/GTK++.h>
+#endif
 
-#include "camera.h"
-#include "macro.h"
-#include "math.h"
-#include "random.h"
-#include "render.h"
-#include "sky.h"
-#include "texture.h"
+#include "Camera.h"
+#include "Macro.h"
+#include "Math.h"
+#include "Random.h"
+#include "Render.h"
+#include "Sky.h"
+#include "Texture.h"
 #include "glTypes.h"
-#include "world.h"
+#include "World.h"
+#include "OS.h"
 
-static CSky*          sky;
+static CSky* sky;
 
 /*-----------------------------------------------------------------------------
 
 -----------------------------------------------------------------------------*/
 
-void SkyRender ()
+void SkyRender()
 {
 
-  if (sky && !RenderFlat ())
-    sky->Render ();
+	if (sky && !RenderFlat())
+		sky->Render();
 
 }
 
@@ -46,12 +56,12 @@ void SkyRender ()
 
 -----------------------------------------------------------------------------*/
 
-void SkyClear ()
+void SkyClear()
 {
 
-  if(sky)
-    delete sky;
-  sky = NULL;
+	if (sky)
+		delete sky;
+	sky = NULL;
 
 }
 
@@ -59,83 +69,82 @@ void SkyClear ()
 
 -----------------------------------------------------------------------------*/
 
-void CSky::Render ()
+void CSky::Render()
 {
+	GLvector angle, position;
 
-  GLvector    angle, position;
-
-  if (!TextureReady ())
-    return;
-  glDepthMask (false);
-  glPushAttrib (GL_POLYGON_BIT | GL_FOG_BIT);
-  glPolygonMode (GL_FRONT_AND_BACK, GL_FILL);
-  glDisable (GL_CULL_FACE);
-  glDisable (GL_FOG);
-  glPushMatrix ();
-  glLoadIdentity();
-  angle = CameraAngle ();
-  position = CameraPosition ();
-  glRotatef (angle.x, 1.0f, 0.0f, 0.0f);
-  glRotatef (angle.y, 0.0f, 1.0f, 0.0f);
-  glRotatef (angle.z, 0.0f, 0.0f, 1.0f);
-  glTranslatef (0.0f, -position.y / 100.0f, 0.0f);
-  glEnable (GL_TEXTURE_2D);
-  glBindTexture(GL_TEXTURE_2D, TextureId (TEXTURE_SKY));
-  glCallList (m_list);
-  glPopMatrix ();
-  glPopAttrib ();
-  glDepthMask (true);
-  glEnable (GL_COLOR_MATERIAL);
-
-
+	if (!TextureReady())
+		return;
+	glDepthMask(false);
+	glPushAttrib(GL_POLYGON_BIT | GL_FOG_BIT);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	glDisable(GL_CULL_FACE);
+	glDisable(GL_FOG);
+	glPushMatrix();
+	glLoadIdentity();
+	angle = CameraAngle();
+	position = CameraPosition();
+	glRotatef(angle.x, 1.0f, 0.0f, 0.0f);
+	glRotatef(angle.y, 0.0f, 1.0f, 0.0f);
+	glRotatef(angle.z, 0.0f, 0.0f, 1.0f);
+	glTranslatef(0.0f, -position.y / 100.0f, 0.0f);
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, TextureId(TEXTURE_SKY));
+	glCallList(m_list);
+	glPopMatrix();
+	glPopAttrib();
+	glDepthMask(true);
+	glEnable(GL_COLOR_MATERIAL);
 }
-
 
 /*-----------------------------------------------------------------------------
 
 -----------------------------------------------------------------------------*/
 
-CSky::CSky ()
+CSky::CSky()
 {
 
-  GLvertex      circle[SKYPOINTS];
-  GLvector      pos;
-  float         angle;
-  int           i;
-  float         size;
-  float         rad;
-  float         lum;
+	GLvertex circle[SKYPOINTS];
+	GLvector pos;
+	float angle;
+	int i;
+	float size;
+	float rad;
+	float lum;
 
-  size = 10.0f;
-  for (i = 0; i < SKYPOINTS; i++) {
-    angle = (float)i / (float)(SKYPOINTS - 1);
-    angle *= 360;
-    angle *= DEGREES_TO_RADIANS;
-    circle[i].position.x = sinf (angle) * size;
-    circle[i].position.y = 0.1f;
-    circle[i].position.z = cosf (angle) * size;
-    circle[i].uv.x = ((float)i / (float)(SKYPOINTS - 1)) * 5.0f;
-    circle[i].uv.y = 0.5f;
-    rad = ((float)i / (SKYPOINTS - 1)) * 180.0f * DEGREES_TO_RADIANS;
-    lum = sinf (rad);
-    lum = (float)pow (lum, 5);
-    circle[i].color = glRgba (lum);
-  }
-  m_list = glGenLists(1);
-  glNewList (m_list, GL_COMPILE);
-  glColor3f (1, 1, 1);
-  
-  glBegin (GL_QUAD_STRIP);
-  for (i = 0; i < SKYPOINTS; i++) {
-    glTexCoord2f (circle[i].uv.x, 0.0f);
-    glVertex3fv (&circle[i].position.x);
-    pos = circle[i].position;
-    pos.y = size / 3.5f;
-    glTexCoord2f (circle[i].uv.x, 1.0f);
-    glVertex3fv (&pos.x);
-  }
-  glEnd ();
-  glEndList();	
-  sky = this;
+	hwnd->glBegin();
+	size = 10.0f;
+	for (i = 0; i < SKYPOINTS; i++)
+	{
+		angle = (float)i / (float)(SKYPOINTS - 1);
+		angle *= 360;
+		angle *= DEGREES_TO_RADIANS;
+		circle[i].position.x = sinf(angle) * size;
+		circle[i].position.y = 0.1f;
+		circle[i].position.z = cosf(angle) * size;
+		circle[i].uv.x = ((float)i / (float)(SKYPOINTS - 1)) * 5.0f;
+		circle[i].uv.y = 0.5f;
+		rad = ((float)i / (SKYPOINTS - 1)) * 180.0f * DEGREES_TO_RADIANS;
+		lum = sinf(rad);
+		lum = (float)pow(lum, 5);
+		circle[i].color = glRgba(lum);
+	}
+	m_list = glGenLists(1);
+	glNewList(m_list, GL_COMPILE);
+	glColor3f(1, 1, 1);
 
+	glBegin(GL_QUAD_STRIP);
+	for (i = 0; i < SKYPOINTS; i++)
+	{
+		glTexCoord2f(circle[i].uv.x, 0.0f);
+		glVertex3fv(&circle[i].position.x);
+		pos = circle[i].position;
+		pos.y = size / 3.5f;
+		glTexCoord2f(circle[i].uv.x, 1.0f);
+		glVertex3fv(&pos.x);
+	}
+	glEnd();
+	glEndList();
+	sky = this;
+	hwnd->glEnd();
 }
--- Sky.h
+++ Sky.h
@@ -3,22 +3,22 @@
 
 struct sky_point
 {
-  GLrgba        color;
-  GLvector      position;
+	GLrgba color;
+	GLvector position;
 };
 
-class CSky 
+class CSky
 {
 private:
-  int                     m_list;
-  int                     m_stars_list;
-  sky_point               m_grid[SKY_GRID][SKY_GRID];
+	int m_list;
+	int m_stars_list;
+	sky_point m_grid[SKY_GRID][SKY_GRID];
 
 public:
-                          CSky ();
-  void                    Render (void);
+	CSky();
+	void Render(void);
 
 };
 
-void SkyRender ();
-void SkyClear ();
+void SkyRender();
+void SkyClear();
