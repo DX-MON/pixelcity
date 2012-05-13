6b0b291061c3e03c5a7955b729aa42ce6d2b2381
dx-mon <dx-mon@users.sourceforge.net>
2012-05-13 13:34:30 +0100

    Added some much needed preprocessor logic in to Macro.h and glTypes.h Reformatted various things including one of the macros in Macro.h to make it more readable General consistancy changes

--- Macro.h
+++ Macro.h
@@ -1,15 +1,35 @@
-#define LIMIT_INTERVAL(interval)  { static unsigned next_update; if (next_update > GetTickCount ()) return; next_update = GetTickCount () + interval;}
+#define LIMIT_INTERVAL(interval)  \
+{ \
+	static unsigned next_update = 0; \
+	if (next_update > GetTickCount()) \
+		return; \
+	next_update = GetTickCount() + interval; \
+}
+
 #define DEGREES_TO_RADIANS        .017453292F
 #define RADIANS_TO_DEGREES        57.29577951F
 #define PI                        ((double)3.1415926535F)
-#define PI2                       PI*PI
+#define PI2                       PI * PI
 #define GRAVITY                   9.5f
+#ifndef CLAMP
 #define CLAMP(a,b,c)              (a < b ? b : (a > c ? c : a))
+#endif
 #define WRAP(x,y)                 ((unsigned)x % y)
 #define SIGN(x)                   (((x) > 0) ? 1 : ((x) < 0) ? -1 : 0)
+#ifndef ABS
 #define ABS(x)                    (((x) < 0 ? (-x) : (x)))
-#define SMALLEST(x,y)             (ABS(x) < ABS(y) ? 0 : x)                
-#define MIN(x,y)                  ((x) < (y) ? x : y)                
-#define MAX(x,y)                  ((x) > (y) ? x : y)                
+#endif
+#define SMALLEST(x,y)             (ABS(x) < ABS(y) ? 0 : x)
+#ifndef MIN
+#define MIN(x,y)                  ((x) < (y) ? x : y)
+#endif
+#ifndef MAX
+#define MAX(x,y)                  ((x) > (y) ? x : y)
+#endif
 #define POW(x,y)                  (float)pow(x,y)
-#define SWAP(a,b)                 {int temp = a;a = b; b = temp;}
+#define SWAP(a,b) \
+{ \
+	int temp = a; \
+	a = b; \
+	b = temp; \
+}
--- glTypes.h
+++ glTypes.h
@@ -1,9 +1,9 @@
 #ifndef glTYPES
-#define glTYPES
+	#define glTYPES
 
-#define GL_CLAMP_TO_EDGE 0x812F
+	#define GL_CLAMP_TO_EDGE 0x812F
 
-#define OPERATORS(type)     \
+	#define OPERATORS(type)     \
   type    operator+  (const type& c); \
   type    operator+  (const float& c);\
   void    operator+= (const type& c);\
@@ -23,77 +23,77 @@
   bool    operator== (const type& c);
 
 
-#define JOINT_MAX_CHILDREN  8
+	#define JOINT_MAX_CHILDREN  8
 
 struct GLquat
 {
-  float       x;
-  float       y;
-  float       z;
-  float       w;
+	float x;
+	float y;
+	float z;
+	float w;
 };
 
 struct GLvector
 {
-  float       x;
-  float       y;
-  float       z;
-  OPERATORS(GLvector);
+	float x;
+	float y;
+	float z;
+	OPERATORS(GLvector);
 };
 
-typedef GLvector       GLvector3;
+typedef GLvector GLvector3;
 
 struct GLvector2
 {
-  float       x;
-  float       y;
-  OPERATORS(GLvector2);
+	float x;
+	float y;
+	OPERATORS(GLvector2);
 };
 
 struct GLrgba
 {
-  float       red;
-  float       green;
-  float       blue;
-  float       alpha;
-  OPERATORS(GLrgba);
+	float red;
+	float green;
+	float blue;
+	float alpha;
+	OPERATORS(GLrgba);
 };
 
 struct GLmatrix
 {
-  float       elements[4][4];
+	float elements[4][4];
 };
 
 struct GLbbox
 {
-  GLvector3   min;
-  GLvector3   max;
+	GLvector3 min;
+	GLvector3 max;
 };
 
 struct GLvertex
 {
-  GLvector3   position;
-  GLvector2   uv;
-  GLrgba      color;
-  int         bone;
+	GLvector3 position;
+	GLvector2 uv;
+	GLrgba color;
+	int bone;
 };
 
 struct GLrect
 {
-  float       left;
-  float       top;
-  float       right;
-  float       bottom;
+	float left;
+	float top;
+	float right;
+	float bottom;
 };
 
 struct GLtriangle
 {
-  int         v1;
-  int         v2;
-  int         v3;
-  int         normal1;
-  int         normal2;
-  int         normal3;
+	int v1;
+	int v2;
+	int v3;
+	int normal1;
+	int normal2;
+	int normal3;
 };
 
 /*
@@ -103,7 +103,7 @@ public:
   unsigned    vertex_count;
   unsigned    triangle_count;
   unsigned    normal_count;
-  GLvertex*   vertex;  
+  GLvertex*   vertex;
   GLvector*   normal;
   GLtriangle* triangle;
 
@@ -112,8 +112,8 @@ public:
   GLtriangle* TriangleAdd (GLtriangle c);
   void        NormalAdd (GLvector n);
   void        VertexAdd (GLvertex v);
-              GLmodel ();
-              ~GLmodel ();
+			  GLmodel ();
+			  ~GLmodel ();
   void        Render ();
   GLbbox      BBox ();
 private:
@@ -140,7 +140,7 @@ struct GLsegment
 class GLanimate
 {
 public:
-                GLanimate ();
+				GLanimate ();
   void          KeyframeAdd (int joint, float time, GLquat q);
   void          TimeSet (float time);
   void          PositionSet (float pos);
@@ -152,58 +152,58 @@ private:
   int           m_segment_count;
   float         m_length;
 };
-*/
+ */
 
-GLbbox    glBboxClear (void);
-GLbbox    glBboxContainPoint (GLbbox box, GLvector point);
-bool      glBboxTestPoint (GLbbox box, GLvector point);
+GLbbox glBboxClear(void);
+GLbbox glBboxContainPoint(GLbbox box, GLvector point);
+bool glBboxTestPoint(GLbbox box, GLvector point);
 
-GLrgba    glRgba (char* string);
-GLrgba    glRgba (float red, float green, float blue);
-GLrgba    glRgba (float luminance);
-GLrgba    glRgba (float red, float green, float blue, float alpha);
-GLrgba    glRgba (long c);
-GLrgba    glRgba (int red, int green, int blue);
-GLrgba    glRgbaAdd (GLrgba c1, GLrgba c2);
-GLrgba    glRgbaSubtract (GLrgba c1, GLrgba c2);
-GLrgba    glRgbaInterpolate (GLrgba c1, GLrgba c2, float delta);
-GLrgba    glRgbaScale (GLrgba c, float scale);
-GLrgba    glRgbaMultiply (GLrgba c1, GLrgba c2);
-GLrgba    glRgbaUnique (int i);
-GLrgba    glRgbaFromHsl (float h, float s, float l);
+GLrgba glRgba(char* string);
+GLrgba glRgba(float red, float green, float blue);
+GLrgba glRgba(float luminance);
+GLrgba glRgba(float red, float green, float blue, float alpha);
+GLrgba glRgba(long c);
+GLrgba glRgba(int red, int green, int blue);
+GLrgba glRgbaAdd(GLrgba c1, GLrgba c2);
+GLrgba glRgbaSubtract(GLrgba c1, GLrgba c2);
+GLrgba glRgbaInterpolate(GLrgba c1, GLrgba c2, float delta);
+GLrgba glRgbaScale(GLrgba c, float scale);
+GLrgba glRgbaMultiply(GLrgba c1, GLrgba c2);
+GLrgba glRgbaUnique(int i);
+GLrgba glRgbaFromHsl(float h, float s, float l);
 
-GLmatrix  glMatrixIdentity (void);
-void      glMatrixElementsSet (GLmatrix* m, float* in);
-GLmatrix  glMatrixMultiply (GLmatrix a, GLmatrix b);
-GLvector  glMatrixTransformPoint (GLmatrix m, GLvector in);
-GLmatrix  glMatrixTranslate (GLmatrix m, GLvector in);
-GLmatrix  glMatrixRotate (GLmatrix m, float theta, float x, float y, float z);
-GLvector  glMatrixToEuler (GLmatrix mat, int order);
+GLmatrix glMatrixIdentity(void);
+void glMatrixElementsSet(GLmatrix* m, float* in);
+GLmatrix glMatrixMultiply(GLmatrix a, GLmatrix b);
+GLvector glMatrixTransformPoint(GLmatrix m, GLvector in);
+GLmatrix glMatrixTranslate(GLmatrix m, GLvector in);
+GLmatrix glMatrixRotate(GLmatrix m, float theta, float x, float y, float z);
+GLvector glMatrixToEuler(GLmatrix mat, int order);
 
-GLquat    glQuat (float x, float y, float z, float w);
-GLvector  glQuatToEuler (GLquat q, int order);
+GLquat glQuat(float x, float y, float z, float w);
+GLvector glQuatToEuler(GLquat q, int order);
 
-GLvector  glVector (float x, float y, float z);
-GLvector  glVectorCrossProduct (GLvector v1, GLvector v2);
-float     glVectorDotProduct (GLvector v1, GLvector v2);
-void      glVectorGl (GLvector v);
-GLvector  glVectorInterpolate (GLvector v1, GLvector v2, float scalar);
-float     glVectorLength (GLvector v);
-GLvector  glVectorNormalize (GLvector v);
-GLvector  glVectorReflect (GLvector3 ray, GLvector3 normal);
+GLvector glVector(float x, float y, float z);
+GLvector glVectorCrossProduct(GLvector v1, GLvector v2);
+float glVectorDotProduct(GLvector v1, GLvector v2);
+void glVectorGl(GLvector v);
+GLvector glVectorInterpolate(GLvector v1, GLvector v2, float scalar);
+float glVectorLength(GLvector v);
+GLvector glVectorNormalize(GLvector v);
+GLvector glVectorReflect(GLvector3 ray, GLvector3 normal);
 
-GLvector2 glVector (float x, float y);
-GLvector2 glVectorAdd (GLvector2 val1, GLvector2 val2);
-GLvector2 glVectorSubtract (GLvector2 val1, GLvector2 val2);
-GLvector2 glVectorNormalize (GLvector2 v);
-GLvector2 glVectorInterpolate (GLvector2 v1, GLvector2 v2, float scalar);
-GLvector2 glVectorSinCos (float angle);
-float     glVectorLength (GLvector2 v);
+GLvector2 glVector(float x, float y);
+GLvector2 glVectorAdd(GLvector2 val1, GLvector2 val2);
+GLvector2 glVectorSubtract(GLvector2 val1, GLvector2 val2);
+GLvector2 glVectorNormalize(GLvector2 v);
+GLvector2 glVectorInterpolate(GLvector2 v1, GLvector2 v2, float scalar);
+GLvector2 glVectorSinCos(float angle);
+float glVectorLength(GLvector2 v);
 
 
 #endif
 
 #ifndef NULL
-#define NULL  0
+	#define NULL  0
 #endif
 
